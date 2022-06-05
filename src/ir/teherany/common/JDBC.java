a552c163f61b250c455520723d2646325ffd4792
Milad Teherany <106352367+miladteherany@users.noreply.github.com>
2022-06-05 18:43:22 +0430

    Update JDBC.java
    Change JDBC

--- src/ir/teherany/common/JDBC.java
+++ src/ir/teherany/common/JDBC.java
@@ -9,9 +9,6 @@ public class JDBC {
     private static BasicDataSource basicDataSource = new BasicDataSource();
 
     static {
-        basicDataSource.setUrl("jdbc:postgresql://156.253.5.223:5432/atm");
-        basicDataSource.setUsername("postgres");
-        basicDataSource.setPassword("myjava123");
         basicDataSource.setMaxTotal(100);
         basicDataSource.setMaxIdle(50);
     }
