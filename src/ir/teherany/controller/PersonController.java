14a559c8f6099c5955e5138f62f0b63f006b7ece
Milad Teherany <miladteherany@gmail.com>
2022-06-08 02:36:09 +0430

    PersonController

--- src/ir/teherany/controller/PersonController.java
+++ src/ir/teherany/controller/PersonController.java
@@ -5,7 +5,6 @@ import ir.teherany.model.service.PersonServiceImpl;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -14,16 +13,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PersonController implements Initializable {
-    @FXML private Button btnSave;
-    @FXML private TextField txtName;
-    @FXML private TextField txtFamily;
-    @FXML private TextField txtAge;
-    @FXML private TextField txtNational;
+    @FXML
+    private Button btnSave;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtFamily;
+    @FXML
+    private TextField txtAge;
+    @FXML
+    private TextField txtNational;
 
     public void save(String name, String family, Integer age, String nationalCode) {
         try {
             Person person = null;
-            if (name.length() != 0 && family.length() != 0) {
+            if (name.length() != 0 && family.length() != 0 && age != 0 && nationalCode.length() != 0) {
                 person = new Person(name, family, age, nationalCode);
             }
             PersonServiceImpl.getInstance().save(person);
@@ -41,7 +45,7 @@ public class PersonController implements Initializable {
                 String familyText = txtFamily.getText();
                 String ageText = txtAge.getText();
                 String nationalText = txtNational.getText();
-                save(nameText, familyText, Integer.valueOf(ageText),nationalText);
+                save(nameText, familyText, Integer.valueOf(ageText), nationalText);
             }
         });
     }
